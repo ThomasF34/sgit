aa9a9b9fab29df01d419d095e276671c48f0db5c
Thomas Falcone <thomas.falcone06@gmail.com>
2019-10-15 12:21:00 +0200

    Add head class

---
+++ src/main/scala/igpolytech/Head.scala
@@ -0,0 +1,37 @@
+package igpolytech
+import java.io.File
+import scala.xml.Node
+
+case class Head(mode: String, commit: Commit) {
+  def save(headPath: String) = FilesIO.saveXml(this.toXml(), headPath)
+
+  def toXml(): Node = <Head mode={mode}>{commit.hash}</Head>
+}
+
+object Head {
+  def exists(branchesPath: String, branchName: String): Boolean =
+    FilesIO.fileExists(s"${branchesPath}$branchName")
+
+  def fromCommitHash(commitHash: String, commitsPath: String): Head =
+    Head("detached", Commit.getCommit(commitHash, commitsPath))
+
+  def fromBranchName(
+      branchName: String,
+      branchesPath: String,
+      commitsPath: String
+  ): Option[Head] = {
+    if (exists(branchesPath, branchName)) {
+      val commitHashOption = FilesIO.getHash(s"${branchesPath}$branchName")
+      commitHashOption
+        .map(commitHash => Commit.getCommit(commitHash, commitsPath))
+        .map(commit => Head("branch", commit))
+    } else None
+  }
+
+  def fromHeadFile(headPath: String, commitsPath: String) = {
+    val xml = FilesIO.loadXml(headPath)
+    val mode = (xml \@ "mode")
+    val commitRef = (xml).text
+    Head(mode, Commit.getCommit(commitRef, commitsPath))
+  }
+}
